6bc7288f992d607e77dcc911e9084c0d76dcb36f
Prasanna Donga <Prasanna.Donga@godigit.com>
2024-07-29 17:14:53 +0530

    position of target element

--- src/PositionOfTargetElement.java
+++ src/PositionOfTargetElement.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class BinarySearch {
-    public static int binarySearch(int arr[], int ele){
+public class PositionOfTargetElement {
+    public static int targetElement(int arr[], int ele){
         int start=0;
         int last=arr.length-1;
         while(start<=last){
@@ -12,28 +12,28 @@ public class BinarySearch {
             else if(arr[mid]>ele){
                 last=mid-1;
             }
-            else {
+            else{
                 return mid;
             }
         }
-       return -1;
+        return -1;
     }
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a length of array");
-        int n=sc.nextInt();
-        int[] arr=new int[n];
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the array length");
+        int n= sc.nextInt();
+        int[] arr = new int[n];
         System.out.println("Enter array elements in sorted order");
-        for(int i=0;i<arr.length;i++ ){
+        for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
+
         }
-        System.out.println("Enter a search element : ");
+        System.out.println("Enter the target element");
         int ele=sc.nextInt();
-        int res= binarySearch(arr, ele);
+
+        int res=targetElement(arr, ele);
         System.out.println(res);
-
-
 
     }
 }
